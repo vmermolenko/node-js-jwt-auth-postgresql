eef4227fc31ddda0be9e47af7ee8cf6826466a44
vmermolenko <vmermolenko.dev@gmail.com>
2021-10-04 23:44:17 +0300

    first commit

---
+++ readme.md
@@ -0,0 +1,2 @@
+https://www.bezkoder.com/node-js-jwt-authentication-postgresql/
+https://www.bezkoder.com/jwt-refresh-token-node-js/
---
+++ server.js
@@ -0,0 +1,67 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+
+
+const app = express();
+
+var corsOptions = {
+  origin: "http://localhost:8081"
+};
+
+app.use(cors(corsOptions));
+
+
+app.use(express.json());
+app.use(express.urlencoded({
+  extended: true
+}));
+
+/*
+// parse requests of content-type - application/json
+app.use(bodyParser.json());
+
+// parse requests of content-type - application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: true }));
+*/
+
+// simple route
+app.get("/", (req, res) => {
+  res.json({ message: "Welcome to bezkoder application." });
+});
+
+// set port, listen for requests
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}.`);
+});
+
+const db = require("./app/models");
+const Role = db.role;
+
+db.sequelize.sync({alter: true}).then(() => {
+  console.log('Drop and Resync Db');
+  //initial();
+});
+
+function initial() {
+  Role.create({
+    id: 1,
+    name: "user"
+  });
+ 
+  Role.create({
+    id: 2,
+    name: "moderator"
+  });
+ 
+  Role.create({
+    id: 3,
+    name: "admin"
+  });
+}
+
+
+// routes
+require('./app/routes/auth.routes')(app);
+require('./app/routes/user.routes')(app);
